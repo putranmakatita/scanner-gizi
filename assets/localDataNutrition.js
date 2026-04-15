a9dc2bb30c6a0b964f2bf4709255114e198c101d
Git <lina@rcelectronic.co.id>
2026-04-15 17:15:36 +0700

    prototype

---
+++ assets/localDataNutrition.js
@@ -0,0 +1,42 @@
+const localDataNutrition = {
+    // --- MINUMAN KEMASAN ---
+    "8999999195801": { nama: "Le Minerale 600ml", gula: 0, lemak: 0, natrium: 5 },
+    "8992741914234": { nama: "Ultra Milk Full Cream 250ml", gula: 10, lemak: 8, natrium: 55 },
+    "8992741970025": { nama: "Ultra Milk Cokelat 250ml", gula: 23, lemak: 5, natrium: 45 },
+    "8996001303863": { nama: "Bear Brand 189ml", gula: 9, lemak: 7, natrium: 95 },
+    "8996001414002": { nama: "Teh Kotak Melati 300ml", gula: 21, lemak: 0, natrium: 15 },
+    "8992761121018": { nama: "Pocari Sweat 350ml", gula: 20, lemak: 0, natrium: 160 },
+    "8993175537110": { nama: "Cimory Yogurt Drink Blueberry 240ml", gula: 16, lemak: 3, natrium: 110 },
+    "8992775313966": { nama: "Aqua 600ml", gula: 0, lemak: 0, natrium: 0 },
+    "8998001101110": { nama: "Teh Pucuk Harum 350ml", gula: 18, lemak: 0, natrium: 15 },
+    "8992741911028": { nama: "Buavita Orange 250ml", gula: 23, lemak: 0, natrium: 10 },
+    "8886001001015": { nama: "Indomilk Steril Plain 189ml", gula: 8, lemak: 6, natrium: 95 },
+    "8991002103509": { nama: "Susu Kental Manis Frisian Flag Gold", gula: 19, lemak: 4, natrium: 35 },
+    "8992695500019": { nama: "Kratingdaeng 150ml", gula: 25, lemak: 0, natrium: 45 },
+
+    // --- MI INSTAN ---
+    "8998866200225": { nama: "Indomie Mi Instan Rasa Ayam Bawang", gula: 3, lemak: 11, natrium: 1330 },
+    "8998866200294": { nama: "Indomie Goreng Spesial", gula: 6, lemak: 12, natrium: 680 },
+    "8998866200317": { nama: "Indomie Kari Ayam", gula: 2, lemak: 14, natrium: 1350 },
+    "8992717111117": { nama: "Mie Sedaap Goreng", gula: 5, lemak: 19, natrium: 820 },
+    "8992717112022": { nama: "Mie Sedaap Soto", gula: 3, lemak: 12, natrium: 1060 },
+    "8998866202168": { nama: "Pop Mie Rasa Baso", gula: 5, lemak: 15, natrium: 720 },
+
+    // --- SNACK & BISKUIT ---
+    "8991102220038": { nama: "Roma Kelapa Biskuit", gula: 6, lemak: 4, natrium: 90 },
+    "8991102111176": { nama: "Roma Malkist Crackers", gula: 3, lemak: 4, natrium: 110 },
+    "8992770055038": { nama: "SilverQueen Milk Chocolate 62g", gula: 28, lemak: 21, natrium: 55 },
+    "8992671510162": { nama: "Beng-Beng 20g", gula: 7, lemak: 5, natrium: 30 },
+    "8998009010308": { nama: "Kopiko Permen Bag", gula: 4, lemak: 1, natrium: 15 },
+    "8992770003008": { nama: "Tango Wafer Cokelat 130g", gula: 12, lemak: 7, natrium: 85 },
+    "8993333333316": { nama: "Chitato Sapi Panggang 68g", gula: 1, lemak: 22, natrium: 420 },
+    "8991102102112": { nama: "Better Sandwich Biscuit", gula: 8, lemak: 6, natrium: 55 },
+    "8992770014028": { nama: "Oreo Vanilla 133g", gula: 11, lemak: 6, natrium: 135 },
+    "8991001501160": { nama: "Potabee Potato Chips Seawit 68g", gula: 1, lemak: 19, natrium: 390 },
+
+    // --- CAMILAN SEHAT / LAINNYA ---
+    "8992752011403": { nama: "Fitbar Chocolate 22g", gula: 4, lemak: 2.5, natrium: 35 },
+    "8997031330018": { nama: "Soyjoy Almond & Chocolate 30g", gula: 6, lemak: 7, natrium: 40 },
+    "8992761001013": { nama: "Heavenly Blush Yogurt Bar 25g", gula: 5, lemak: 3, natrium: 25 },
+    "8992717151014": { nama: "Top Coffee Gula Aren 22g", gula: 13, lemak: 2.5, natrium: 30 }
+};
